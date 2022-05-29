481620075c618b0d6cfc6dc7e63e31027b9e93c5
Савосин Антон <anle@list.ru>
2022-05-29 21:39:53 +0300

    Савосин Антон

--- push-sender/src/main/kotlin/ru/netology/pusher/main.kt
+++ push-sender/src/main/kotlin/ru/netology/pusher/main.kt
@@ -21,9 +21,24 @@ fun main() {
           "userId": 1,
           "userName": "Vasiliy",
           "postId": 2,
-          "postAuthor": "Netology"
+          "postAuthor": "Netology",
+          "postContent": "We help you grow in your profession and open new career horizons.
+          
+          Netology is four levels of education:
+
+          🔹 For beginners who want to master an actual profession from scratch
+          🔹 For professionals who seek to grow in their careers
+          🔹 For managers who aim to strengthen leadership skills
+          🔹 And for companies that need to train a team
+
+          Marketing, design, programming, soft skills, MBA, project management, analytics and Data Science — choose what is closer to you and come to us for knowledge and growth.
+
+          Change is always the right choice 👌
+          http://netolo.gy/fEU
+          8 (800) 301-39-69
+          Varshavskoe shosse, 1, p. 6, 1st floor, office 105A, Moscow"
         }""".trimIndent())
-        .setToken(token)
+        .setToken("fBnheoqDSMiyg7Baek2U1r:APA91bHg2J3hLkWWcNQ0K2u44kpC2_ovTfePBUdhEmjBVPl-zF2Vpu04-tc7pMX8ZvA2J5w-do8ugB5aBiKBpKt5W8WZ0If9PxKSiAklsZ_ZJILU8nffn0gsKNtJp_NAgE9t2ze7z0XK")
         .build()
 
     FirebaseMessaging.getInstance().send(message)
